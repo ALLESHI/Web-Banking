cb15e65ba39865702afada374ccc3bfc9a6df4a3
ardit <arditlleshi@hotmail.com>
2022-11-30 10:20:32 +0100

    Commit and Push

--- src/main/java/com/example/Online/Banking/model/User.java
+++ src/main/java/com/example/Online/Banking/model/User.java
@@ -1,13 +1,9 @@
 package com.example.Online.Banking.model;
-
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import java.util.List;
 
-@Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,4 +22,60 @@ public class User {
 
     @OneToMany(mappedBy="user")
     private List<BankAccount> bankAccounts;
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname.substring(0, 1).toUpperCase() + surname.substring(1).toLowerCase();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email.toLowerCase();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<BankAccount> getBankAccounts() {
+        return bankAccounts;
+    }
+
+    public void setBankAccounts(List<BankAccount> bankAccounts) {
+        this.bankAccounts = bankAccounts;
+    }
 }
